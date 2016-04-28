43fd0aeb29ff08471ce82d795c8e7f468cac3a45
hyg <934533623@qq.com>
2016-04-28 11:49:57 +0800

    Initial Commit

---
+++ HYGPage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HYGPage
+//
+//  Created by 侯英格 on 16/4/28.
+//  Copyright © 2016年 侯英格. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
